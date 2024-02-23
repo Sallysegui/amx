647d5f2bdf23ea485a419620c4b68b395da0e781
Salud <sallysegui10@gmail.com>
2024-02-23 14:11:43 +0100

    amxPre27

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 //import './index.css'
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-0W9VMKMX1E");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
